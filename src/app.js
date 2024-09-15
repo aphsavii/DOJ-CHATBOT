3bdf4ba6315a37bdd9cb16ee8cca5b3c94d4d1dc
Avinash Kumar <avinash.k95347@gmail.com>
2024-09-16 00:07:40 +0530

    fix cors for nginx

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import { createServer } from 'http';
-import cors from 'cors';
+// import cors from 'cors';
 const app = express();
 const httpServer = createServer(app);
 
 app.use(express.static('public'));
 app.use(express.json({limit:'16kb'}));
 app.use(express.urlencoded({ extended: true, limit:'16kb' }));
-app.use(cors({origin: '*'}));
+// app.use(cors({origin: '*'}));
 // Routes import
 import { queryRouter } from './routes/query.routes.js';
 import {embedRouter} from './routes/embed.routes.js';
